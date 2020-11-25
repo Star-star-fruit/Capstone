109a9903440ce2851fe3962690f53b5cce0ec848
Elisa <elisalevet@hotmail.com>
2020-11-25 12:39:04 -0500

    testing

---
+++ server/nlp/googleApplication.js
@@ -0,0 +1,1 @@
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/elisalevetlavin/Desktop/Capstone-a313a2660b52.json"
---
+++ server/nlp/googleCloud.js
@@ -0,0 +1,2 @@
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/elisalevetlavin/Desktop/Capstone-99b658775b87.json"
+
