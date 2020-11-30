ea84d7a3b3f604abcdfa78f758988308d7fc4ad3
Prakruti PK <prakruti.pk@gmail.com>
2020-11-29 20:41:41 -0500

    WORKING ROUTE! Returns SA and words

--- server/nlp/test.js
+++ server/nlp/test.js
@@ -1,4 +1,3 @@
-
 async function quickstart() {
   //   // Imports the Google Cloud client library
   //   const language = require('@google-cloud/language')
@@ -34,7 +33,7 @@ async function quickstart() {
   /**
    * TODO(developer): Uncomment the following line to run this code.
    */
-  // const text = 'Your text to analyze, e.g. Hello, world!';
+  const text = `Hello, it's me. I was wondering if after all these years you'd like to meet to go over everything. They say that time's supposed to heal ya, but I ain't done much healing. Hello, can you hear me? I'm in California dreaming about who we used to be when we were younger and free. I've forgotten how it felt before the world fell at our feet. There's such a difference between us and a million miles. Hello from the other side, I must've called a thousand times to tell you I'm sorry for everything that I've done, but when I call you never seem to be home. Hello from the outside; at least I can say that I've tried to tell you I'm sorry for breaking your heart, but it don't matter, it clearly doesn't tear you apart anymore. Hello, how are you? It's so typical of me to talk about myself, I'm sorry.`
 
   // Prepares a document, representing the provided text
   const document = {
