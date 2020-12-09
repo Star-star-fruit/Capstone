1f96b32e9b3bac5b1ed497f2e4be8a30bc1f90c7
Elisa <elisalevet@hotmail.com>
2020-12-09 18:58:24 -0500

    feat(newDeploy: components/home, signup, textedtor, api/helper, users, index) implemented alert messages for login and signup and helper function to protect our routes

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -17,6 +17,11 @@ const User = db.define('user', {
       notEmpty: true
     }
   },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
